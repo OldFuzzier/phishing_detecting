fea143b3fddc413b89f6d14cd7dc71a4fcb6c932
fuzzier <630533571@qq.com>
2017-04-24 09:54:26 +0800

    test KNN

--- phishing/classifier.py
+++ phishing/classifier.py
@@ -9,6 +9,23 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
+import feature_test as ft
+
+
+url1 = "http://www.luxinnerwear.com/javascript_gallery/alibaba/alibaba/login.alibaba.com.php?email=abuse@madecenter.com"
+url2 = "http://www.Confirme-paypal.com/"
+url3 = "http://www.mmch.co.in/font/index2.html?email=abuse@sfc-ksa.com"
+url4 = "http://xxxxx/abc?name=admin&password=admin"
+url5 = "http://docs.restala.com/LatestG/file/ServiceLoginAuth.php?cmd=?LOB=RBGLogon&amp;_pageLabel=page_logonform&amp;secured_buyer_page"
+url6 = "http://www.legitimate.com//http://www.phishing.com"
+url7 = "http://173.236.239.124/wp-content/themes/twentyfourteen/css/alibaba/index.php?email=abuse@houstonlinksmagazine.com"
+url8 = "http://www.legitimate.com//http://www.phishing.com"
+url9 = "http://www.cclpgms.com/js/?_Acess_Tooken&amp;Acirc;????????????????????????????????792e070ef1e2a9747e63dd241bb32a87792e070ef1e2a9747e63dd241bb32a"
+url10 = "http://3designcenter.com/blog/wp-admin/network/other/index.html?.rand&amp;us.battle.net/login/en"
+url11 = "http://shop.dominion.dn.ua/templates/redirect/smiles_motivo_para_sorrir_/?https://www.smiles.com.br/promocoes/redireciona/cadastrar/novapromocao/"
+url12 = 'https://logn.tabao.com/member/loin.jhtml?redirectURL=http%3A%2F%2Fbuy.tobao.com%2Fauction%2Fbuy_now.htm%3Fphone%3D13718868748%26tccdetailc%3Dwt_one_menu%26action%3Dbuynow%252FPhoneEcardBuyNowAction%26item_id_num%3D9559209412%26from%3Dtcc%26event_submit_do_buy%3D1'
+url13 = 'https://auth.alispay.com/logsin/'
+
 
 nbc_1 = Pipeline([
     ('clf', MultinomialNB())
@@ -67,6 +84,21 @@ def main():
     return
 
 
+def test_KNN():
+    dataset = load_data()
+    data = dataset[:, :12]
+    target = dataset[:, 12]
+    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
+    clf = KNeighborsClassifier(algorithm='kd_tree')
+    clf.fit(x_train, y_train)
+    return clf
+
 if __name__ == '__main__':
-    main()
+    KNN_clf = test_KNN()
+    for i in range(1, 14):
+        url_np = np.array(ft.set_feature(eval('url'+str(i))))
+        print KNN_clf.predict(url_np)
+    print len(url_np)
+
+
 
